8ca4a385124df7c1c0a044cd5a51f2b344618f2b
0Gravity94 <leesooyun94@gmail.com>
2023-03-07 16:17:33 +0800

    working on footer

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 import "../Styles/App.css";
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Footer />
     </div>
   );
 }
